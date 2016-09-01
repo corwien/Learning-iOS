f11e94b487cff1a9776420a049fb848ffc37875f
kaiyi wang <kaiyiwang@kaiyideMacBook-Pro.local>
2016-09-02 01:14:50 +0800

    scrollView test

--- 05-UIScrollView/05-UIScrollView/ViewController.m
+++ 05-UIScrollView/05-UIScrollView/ViewController.m
@@ -20,10 +20,42 @@
     // 创建UIScrollView
     UIScrollView *scrollView = [[UIScrollView alloc] init];
     
-    // 可视范围，能看的见的范围是 320，460
-    scrollView.frame = CGRectMake(0, 0, 320, 640);
+    // 可视范围，能看的见的范围是 400，460
+    // 获取屏幕的宽高
+    CGFloat screenW = [UIScreen mainScreen].bounds.size.width;
+    CGFloat screenH = [UIScreen mainScreen].bounds.size.height;
+    scrollView.frame = CGRectMake(0, 0, screenW , screenH);
     scrollView.backgroundColor = [UIColor grayColor];
     [self.view addSubview:scrollView];
+    
+    
+     // 拿到图片的宽度和高度
+    UIImageView *imageView = [[UIImageView alloc] init];
+    imageView.image = [UIImage imageNamed:@"myImage.jpg"];
+    CGFloat imgW = imageView.image.size.width;
+    CGFloat imgH = imageView.image.size.height;
+    
+    // 设置控件位置大小
+    imageView.frame = CGRectMake(0, 0, imgW, imgH);
+    [scrollView addSubview:imageView];
+    
+     // 设置UIScrollView的滚动范围（内容大小）
+    scrollView.contentSize = imageView.image.size;
+    
+    // 隐藏滚动条
+    scrollView.showsVerticalScrollIndicator = NO;
+    scrollView.showsHorizontalScrollIndicator = YES;
+    
+    // 设置弹簧效果
+    // scrollView.bounces = NO; // 去掉弹簧效果
+    
+    
+    // 结构体类型    make快速创建                up left down right
+   // scrollView.contentInset = UIEdgeInsetsMake(50, 0, 0, 0);
+
+    
+    
+    
 }
 
 
